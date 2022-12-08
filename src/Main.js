4f60af83f735a7d4411ae7e6bec504754395e1b9
Gian <gian.defaria@hotmail.com>
2022-12-08 01:13:56 -0300

    alterações no box de benefícios e alteração visual do input

--- src/Main.js
+++ src/Main.js
@@ -57,25 +57,25 @@ function Main() {
               <img src={recognition} alt="brand recognition" />
             </div>  
             <h2>Brand Recognition</h2>
-            <p>Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content</p>
+            <p>Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.</p>
 
           </div>
             
-          <div className="box--beneficies  margin--5">
+          <div className="box--beneficies  margin--4">
             <div className="icon--design">
               <img src={records} alt="brand recognition" />
             </div>  
-            <h2>Brand Recognition</h2>
-            <p>Bost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content</p>
+            <h2>Detailed Records</h2>
+            <p>Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
 
           </div>
 
-          <div className="box--beneficies margin--10">
+          <div className="box--beneficies margin--8">
             <div className="icon--design">
               <img src={customizable} alt="brand recognition" />
             </div>  
-            <h2>Brand Recognition</h2>
-            <p>Bost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content</p>
+            <h2>Fully Customizable</h2>
+            <p>Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.</p>
 
           </div>
 
