c85b8f5453413d4b1364e5066dd1c4b79264a05f
Gian <gian.defaria@hotmail.com>
2022-12-11 15:42:02 -0300

    adicionei o menu no mobile e ajustei o visual para o acesso do site no celular

--- src/Headers.js
+++ src/Headers.js
@@ -26,10 +26,30 @@ function test() {
 
             </section>
 
-            <img className='menu--icon' src={menu} alt="menu" />
+            <img className='menu--icon' src={menu} alt="menu" onClick={menuOn} />
 
         </header>
   );
+
+        function menuOn() {
+            let navDisplay = document.querySelector('.nav--links');
+            let loginDisplay = document.querySelector('.login--acess');
+            let loginboxDisplay = document.querySelector('.login--box');
+
+            if (navDisplay.classList.value == 'nav--links') {
+                navDisplay.classList.add ('nav--links--display--mobile');
+                loginDisplay.classList.add  ('login--acess--display--mobile');
+                loginboxDisplay.classList.add ('login--box--display--mobile')
+                
+            } else {
+                navDisplay.classList.remove ('nav--links--display--mobile');
+                loginDisplay.classList.remove ('login--acess--display--mobile');
+                loginboxDisplay.classList.remove ('login--box--display--mobile')
+            }
+
+        }
+
+  
 }
 
 export default test;
